a687b0f1dd1280b8557efbb013109c601a4b1276
Julian Ramos <jd.ramosg1@uniandes.edu.co>
2025-10-25 18:51:10 -0500

    jeje funciona el inicio

--- mynewapp/src/app/app-module.ts
+++ mynewapp/src/app/app-module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { SeriesModule } from './series/series-module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SeriesModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ mynewapp/src/app/series/series-list/series-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-series-list',
+  standalone: false,
+  templateUrl: './series-list.html',
+  styleUrl: './series-list.css',
+})
+export class SeriesList {
+
+}
